618120313e6fce27497a63ec1fddef8fe60bafda
Bender Man <bspafford05@gmail.com>
2025-11-11 15:48:17 -0500

    Added transmission for glass improved some of the GPU padding for the SSBOs

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -47,7 +47,7 @@ int Main::createWindow() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(stuff::screenSize.x, stuff::screenSize.y, "Raytracing", nullptr, nullptr);
+	window = glfwCreateWindow(screenSize.x, screenSize.y, "Raytracing", nullptr, nullptr);
 
 	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window\n";
@@ -56,12 +56,13 @@ int Main::createWindow() {
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, Main::FramebufferSizeCallback);
 
 	if (!gladLoadGL()) {
 		return -1;
 	}
 
-	glViewport(0, 0, stuff::screenSize.x, stuff::screenSize.y);
+	glViewport(0, 0, screenSize.x, screenSize.y);
 
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
@@ -72,17 +73,15 @@ int Main::createWindow() {
 	Start();
 
 	// setup quad texture for Compute shader to write on
-	const unsigned int TEXTURE_WIDTH = stuff::screenSize.x, TEXTURE_HEIGHT = stuff::screenSize.y;
-	unsigned int texture;
-	glGenTextures(1, &texture);
+	glGenTextures(1, &quadTexture);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glBindTexture(GL_TEXTURE_2D, quadTexture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
-	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenSize.x, screenSize.y, 0, GL_RGBA, GL_FLOAT, NULL);
+	glBindImageTexture(0, quadTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	setupQuad();
@@ -94,7 +93,7 @@ int Main::createWindow() {
 	for (int i = 0; i < BVHList.size(); i++) {
 		GPUBoundingBox& boundingBox = BVHList[i];
 		Box* box = new Box(boundingBox.minLoc, boundingBox.maxLoc);
-		if (boundingBox.isLeaf.x) // second half of list, meaning its last node
+		if (boundingBox.isLeaf) // second half of list, meaning its last node
 			box->setColor(glm::vec3(0, 1, 1));
 	}
 
@@ -119,19 +118,19 @@ int Main::createWindow() {
 			computeShader->setVec3("camOrientation", camera->Orientation);
 			computeShader->setMat4("view", camera->view);
 			computeShader->setMat4("invView", glm::inverse(camera->view));
-			computeShader->setFloat("aspect", stuff::screenSize.x / stuff::screenSize.y);
+			computeShader->setFloat("aspect", screenSize.x / screenSize.y);
 			computeShader->setFloat("fov", camera->FOVdeg);
 			//computeShader->setVec3("lightDir", glm::normalize(glm::vec3(cos(time), 0.75, sin(time))));
 			computeShader->setVec3("lightDir", glm::normalize(glm::vec3(1.f, 0.75f, -0.5f)));
 			
-			glDispatchCompute((TEXTURE_WIDTH + 15) / 16, (TEXTURE_HEIGHT + 15) / 16, 1); // simple strat to make somethiung like width = 500 into 512 which is divisible by 16
+			glDispatchCompute((screenSize.x + 15) / 16, (screenSize.y + 15) / 16, 1); // simple strat to make somethiung like width = 500 into 512 which is divisible by 16
 			// make sure writing to image has finished before read
 			glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
 			quadShader->Activate();
 			quadShader->setInt("tex", 0);
 			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, texture);
+			glBindTexture(GL_TEXTURE_2D, quadTexture);
 			renderQuad();
 
 			//shader->Activate();
@@ -159,21 +158,22 @@ void Main::Start() {
 	quadShader = new Shader("quadShader.vert", "quadShader.frag");
 	computeShader = new ComputeShader("shader.comp");
 
-	camera = new Camera(stuff::screenSize.x, stuff::screenSize.y, glm::vec3(0, 0, 5));
+	camera = new Camera(screenSize.x, screenSize.y, glm::vec3(0, 0, 5));
 	sphere = new Model("models/sphere/sphere.gltf");
 	//sphere = new Model("models/bunny/3k.gltf");
 	cube = new Model("models/cube/cube.gltf");
 	sphereFlat = new Model("models/sphere1/sphere.gltf");
+	new Model("models/glassSphere/sphere.gltf");
 }
 
 void Main::Update(float deltaTime) {
 	shader->Activate();
-	camera->Update(deltaTime, window, shader);
+	camera->Update(deltaTime, window, shader, screenSize);
 }
 
 void Main::Draw(Shader* shader) {
 	shader->Activate();
-
+	camera->Matrix(shader, "camMatrix");
 	shader->setMat4("model", glm::mat4(1));
 	shader->setMat4("translation", glm::mat4(1));
 	shader->setMat4("rotation", glm::mat4_cast(glm::quat(1, 0, 0, 0)));
@@ -183,4 +183,16 @@ void Main::Draw(Shader* shader) {
 		box->draw(shader);
 	//for (Model* model : Model::instances)
 		//model->Draw(shader, camera);
+}
+
+void Main::FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	screenSize = { float(width), float(height) };
+	glfwSetWindowSize(window, screenSize.x, screenSize.y);
+
+	// update texuture size
+	glViewport(0, 0, screenSize.x, screenSize.y);
+	glBindTexture(GL_TEXTURE_2D, quadTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenSize.x, screenSize.y, 0, GL_RGBA, GL_FLOAT, nullptr);
+	glBindImageTexture(0, quadTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
